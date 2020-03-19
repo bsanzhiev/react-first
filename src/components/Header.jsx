7efde1e38586a80ed4747c10deb51a7dd7a4b2fe
Bator JS <bsanzhiev@yandex.ru>
2020-03-19 17:15:20 +0800

    css модули (14 урок)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
         <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/1024px-Spotify_logo_without_text.svg.png'></img>
         </header>
     )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
-    return (
-        <div className='content'>
-            <div>
-                <img src='https://cdn.vox-cdn.com/thumbor/lCAHfXW-NhmS4tF2gBZmGuU2el4=/0x0:800x600/1200x800/filters:focal(266x304:394x432)/cdn.vox-cdn.com/uploads/chorus_image/image/55227337/Bliss.1497349801.png'></img>
-            </div>
+  return (
+    <div className={s.content}>
+      <div>
+        <img src='https://cdn.vox-cdn.com/thumbor/lCAHfXW-NhmS4tF2gBZmGuU2el4=/0x0:800x600/1200x800/filters:focal(266x304:394x432)/cdn.vox-cdn.com/uploads/chorus_image/image/55227337/Bliss.1497349801.png'></img>
+      </div>
+      <div>
+        <img src='https://cutecatshq.com/wp-content/uploads/2017/03/citty-puss.jpg'></img>
+      </div>
+      <div>
+        My Posts
         <div>
-          <img src='https://cutecatshq.com/wp-content/uploads/2017/03/citty-puss.jpg'></img>
+          New Post
         </div>
-        <div>
-          My Posts
-          <div>
-            New Post
-          </div>
+        <div className={s.posts}>
+          <div className={s.item}>
           Post 1
-        </div>
-        <div>
+          </div>
+          <div className={s.item}>
           Post 2
+          </div>
         </div>
       </div>
-    )
+    </div>
+  )
 };
 
 export default Profile;
