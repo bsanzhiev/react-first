859e560c93f3488f273ed765fa2bd5510928e300
Bator JS <bsanzhiev@yandex.ru>
2020-03-29 23:17:21 +0800

    wallpaper

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () => {
   return (
     <div className={s.content}>
       <div>
-        <img src='https://melmagazine.com/wp-content/uploads/2019/01/vapormall-1280x533.jpg'></img>
+        <img src='https://melmagazine.com/wp-content/uploads/2019/01/vapormall-1280x533.jpg' width='1200'></img>
       </div>
       <div>
         <img src='https://cutecatshq.com/wp-content/uploads/2017/03/citty-puss.jpg' width='200px'></img>
