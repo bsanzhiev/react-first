301496db207156988f939a62e32d927c2ba20ad4
bsanzhiev <bsanzhiev@outlook.com>
2022-08-22 23:23:02 +0800

    removed trash

--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
-//import { BrowserRouter } from "react-router-dom";
-//import store from "./redux/redux_store";
-//import StoreContext from './StoreContext';
-//import { Provider } from "react-redux";
 import "./index.css";
-//import App from "./App";
 import WrappedApp from "./App";
 
 ReactDOM.render(<WrappedApp />, document.getElementById("root"));
-
-/* Подключили хранилище состояния к приложению */
-/* ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById("root")
-); */
 
 serviceWorker.unregister();
